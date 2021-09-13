0a5125df11472b26a6586a0ede2c69469ac05fc6
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-09-12 20:47:57 -0400

    solution for convert boolean to string

---
+++ 8kyu/convertBoolean.js
@@ -0,0 +1,8 @@
+function boolToWord( bool ){
+    if(bool === true){
+        return 'True'
+    }else if(bool === false){
+        return 'False'
+    }
+  }
+//   https://www.codewars.com/kata/53369039d7ab3ac506000467
