b80d919f52564b678847fbdac16350181cd9e59d
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-09-15 22:20:17 -0400

    solution for reversed strings

---
+++ 8kyu/reversedString.js
@@ -0,0 +1,8 @@
+function solution(str){
+    const array = str.split('')
+    const reverse = array.reverse()
+    const join = reverse.join('')
+    return join
+    }
+    
+// https://www.codewars.com/kata/5168bb5dfe9a00b126000018
