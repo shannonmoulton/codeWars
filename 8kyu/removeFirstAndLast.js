5f49bbf68221ed963ddaa60d71f17f0aa9ea72f5
Shannon Moulton <89002541+shannonmoulton@users.noreply.github.com>
2021-09-21 09:27:54 -0400

    Update removeFirstAndLast.js

--- 8kyu/removeFirstAndLast.js
+++ 8kyu/removeFirstAndLast.js
@@ -1,5 +1,7 @@
-function repeatStr (n, s) {
-    var s = s.repeat(n)
-    return s;
-  }
+function removeChar(str){
+ var newStr = str.slice(1, -1)
+ return newStr
+};
+
+
 //https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
