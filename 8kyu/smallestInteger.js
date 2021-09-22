64f4ab542727d624c5ac2a094dbab7b859cd6420
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-09-21 23:19:17 -0400

    soultion for find the smallest integer in an array

---
+++ 8kyu/smallestInteger.js
@@ -0,0 +1,8 @@
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      var smallestInteger = Math.min(...args)
+      return smallestInteger
+    }
+  }
+
+//https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
