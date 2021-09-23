bef70fcb432709f37335975ee432e2e574d29f4d
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-09-22 20:59:58 -0400

    solution for grasshopper summation

---
+++ 8kyu/grasshopper.js
@@ -0,0 +1,9 @@
+var summation = function (num) {
+    let sum = 0;
+    for (var i = 1; i <= num; i++){
+     sum += i;
+   }
+   return sum
+  }
+
+//https://www.codewars.com/kata/55d24f55d7dd296eb9000030
