54b8bfd9cae10ab262d9319a83b2975cdd4c9037
Resilient Coders <resilientcoders@Resilients-MacBook-Air.local>
2021-09-25 21:58:29 -0400

    soultion to counting sheep codewars challenge

---
+++ 8kyu/countingsheep.js
@@ -0,0 +1,8 @@
+function countSheeps(arrayOfSheep){
+    let count = 0;
+    for(let element of arrayOfSheep )
+    if(element === true) count++;
+    return count;
+  }
+
+//https://www.codewars.com/kata/54edbc7200b811e956000556
