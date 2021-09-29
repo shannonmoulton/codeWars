9893c3ad2d6d0a78ac0e119555b2fa724a8046b3
Shannon Moulton <moulton.shannon@gmail.com>
2021-09-28 21:23:48 -0400

    solution to code wars

---
+++ 8kyu/basicMathOp.js
@@ -0,0 +1,14 @@
+function basicOp(operation, value1, value2)
+{
+  if(operation === '+') {
+    return value1 + value2
+  }else if(operation === '-') {
+    return value1 - value2
+  }else if(operation === '*') {
+    return value1 * value2
+  }if(operation === '/') {
+  return value1 / value2
+  } 
+}
+
+//https://www.codewars.com/kata/57356c55867b9b7a60000bd7
---
+++ 8kyu/stringToNumber.js
@@ -0,0 +1,4 @@
+var stringToNumber = function(str){
+    return parseInt(str);
+  }
+//https://www.codewars.com/kata/544675c6f971f7399a000e79
