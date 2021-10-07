81af756c1e884e986c6471cdaaebfa8afbb47e6c
Shannon Moulton <moulton.shannon@gmail.com>
2021-10-07 17:22:11 -0400

    solution for codeWar challenge

---
+++ 8kyu/lostWithoutAmap.js
@@ -0,0 +1,4 @@
+function maps(arr) {
+  return arr.map((n) => n * 2);
+}
+//https://www.codewars.com/kata/57f781872e3d8ca2a000007e
---
+++ 8kyu/meanOfArray.js
@@ -0,0 +1,4 @@
+function getAverage(arr) {
+  return Math.floor((averageOfArr = arr.reduce((a, b) => a + b) / arr.length));
+}
+//https://www.codewars.com/kata/563e320cee5dddcf77000158
