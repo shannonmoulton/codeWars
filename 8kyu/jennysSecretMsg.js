559f8c8b50c59b81709975dda149b5b07338bb1a
Shannon Moulton <moulton.shannon@gmail.com>
2021-10-06 21:01:14 -0400

    codeWars challenge solutions

---
+++ 8kyu/jennysSecretMsg.js
@@ -0,0 +1,8 @@
+function greet(name) {
+  if (name === "Johnny") {
+    return "Hello, my love!";
+  } else {
+    return `Hello, ${name}!`;
+  }
+}
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390
---
+++ 8kyu/needleInTheHaystack.js
@@ -0,0 +1,5 @@
+function findNeedle(haystack) {
+  const needle = haystack.find((needle) => "needle");
+  return `found the needle at position ${haystack.indexOf("needle")}`;
+}
+//https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
