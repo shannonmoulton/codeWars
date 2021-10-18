29560e1eab4aee34cd172acdb93162a99380f55b
Shannon Moulton <moulton.shannon@gmail.com>
2021-10-17 23:36:30 -0400

    solution to count the monkey's code war challenge

---
+++ 8kyu/countTheMonkey.js
@@ -0,0 +1,8 @@
+function monkeyCount(n) {
+  let monkeys = [];
+  for (let i = 1; i < n + 1; i++) {
+    monkeys.push(i);
+  }
+  return monkeys;
+}
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
