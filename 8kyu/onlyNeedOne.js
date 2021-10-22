662d0272a07b78a0b20573fd4ecec2bd6e82dc54
Shannon Moulton <moulton.shannon@gmail.com>
2021-10-21 22:43:48 -0400

    solution to code war challenge

---
+++ 8kyu/onlyNeedOne.js
@@ -0,0 +1,11 @@
+function check(arr, x) {
+  return arr.includes(x) ? true : false;
+}
+//OR
+
+if (arr.includes(x)) {
+  return true;
+} else {
+  return false;
+}
+//https://www.codewars.com/kata/57cc975ed542d3148f00015b
