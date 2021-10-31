0176f9fae12f2f2ddfa2dd5ed951665648fd2953
Shannon Moulton <moulton.shannon@gmail.com>
2021-10-30 20:04:08 -0400

    solution to code war challenge

---
+++ 8kyu/highestAndLowest.js
@@ -0,0 +1,5 @@
+function highAndLow(numbers){
+    let nums = numbers.split(' ')
+   return `${Math.max(...nums)} ${Math.min(...nums)}` 
+  }
+//https://www.codewars.com/kata/554b4ac871d6813a03000035
