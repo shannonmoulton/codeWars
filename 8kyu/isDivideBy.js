9190a518490d40b5d8a397d3411179479ab6847e
Shannon Moulton <moulton.shannon@gmail.com>
2021-11-11 14:33:50 -0500

    solution to code war

---
+++ 8kyu/isDivideBy.js
@@ -0,0 +1,7 @@
+function isDivideBy(number, a, b) {
+    if(number % a === 0 && number % b === 0){
+      return true 
+    }else 
+      return false
+   }
+//https://www.codewars.com/kata/5a2b703dc5e2845c0900005a/train/javascript
