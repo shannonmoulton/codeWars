10f1ae0de5558169b2206686711cc6c73f6b66b0
Shannon Moulton <moulton.shannon@gmail.com>
2021-11-29 11:03:41 -0500

    codewar solution

---
+++ 8kyu/multiplicationTableForNumber.js
@@ -0,0 +1,4 @@
+function multiTable(number) {
+    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(e => `${e} * ${number} = ${e * number}`).join('\n')
+}
+//https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce
