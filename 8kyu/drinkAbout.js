73e37d64ecab953c617ac35d361b3b320ff48e85
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-08 10:56:05 -0500

    codewar solution

---
+++ 8kyu/drinkAbout.js
@@ -0,0 +1,7 @@
+function peopleWithAgeDrink(old) {
+    return old < 14 ? 'drink toddy'
+        : old < 18 ? 'drink coke'
+            : old < 21 ? 'drink beer'
+                : 'drink whisky';
+};
+//https://www.codewars.com/kata/56170e844da7c6f647000063
