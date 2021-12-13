4e80f4e2bde95fa7d1d4b58ceaafd1186cd7efe8
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-13 13:33:04 -0500

    codewar solution

---
+++ 8kyu/removingElements.js
@@ -0,0 +1,7 @@
+function removeEveryOther(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        arr.splice(i, 1)
+    }
+    return arr
+}
+//https://www.codewars.com/kata/5769b3802ae6f8e4890009d2
