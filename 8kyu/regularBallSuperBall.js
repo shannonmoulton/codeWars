f0b85709db6ae781e28e8b880873f4e1524f8d8d
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-14 13:32:03 -0500

    codewar solution

---
+++ 8kyu/regularBallSuperBall.js
@@ -0,0 +1,8 @@
+var Ball = function (ballType) {
+    if (ballType) {
+        this.ballType = ballType
+    } else {
+        this.ballType = "regular"
+    }
+};
+//https://www.codewars.com/kata/53f0f358b9cb376eca001079
