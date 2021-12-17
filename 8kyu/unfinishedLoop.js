79a5f0e81794f09616def3578146f3bc6ba70c9d
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-17 11:19:40 -0500

    codewar solution

---
+++ 8kyu/unfinishedLoop.js
@@ -0,0 +1,10 @@
+function createArray(number) {
+    var newArray = [];
+
+    for (var counter = 1; counter <= number; counter++) {
+        newArray.push(counter);
+    }
+
+    return newArray;
+}
+//https://www.codewars.com/kata/55c28f7304e3eaebef0000da
