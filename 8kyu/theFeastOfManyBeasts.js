d015fa38cd64d47e1052c19eff644f7aa60ea21b
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-18 11:18:44 -0500

    solution to codewar

---
+++ 8kyu/theFeastOfManyBeasts.js
@@ -0,0 +1,4 @@
+function feast(beast, dish) {
+    return beast[0] === dish[0] && beast.charAt(beast.length - 1) === dish.charAt(dish.length - 1)
+}
+//https://www.codewars.com/kata/5aa736a455f906981800360d
