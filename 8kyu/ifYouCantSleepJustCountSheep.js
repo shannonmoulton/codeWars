5b4ae1b41ed157f33772f13e9780923b7baa7902
Shannon Moulton <moulton.shannon@gmail.com>
2021-12-29 13:48:22 -0500

    codewar solution

---
+++ 8kyu/ifYouCantSleepJustCountSheep.js
@@ -0,0 +1,8 @@
+var countSheep = function (num) {
+    let sheepCount = ""
+    for (let i = 1; i < num + 1; i++) {
+        sheepCount += i + ' sheep...'
+    }
+    return sheepCount
+}
+//https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
