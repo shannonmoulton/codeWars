08bc8591adad5fd303189880ad7898de14174c42
Shannon Moulton <moulton.shannon@gmail.com>
2022-01-07 21:53:42 -0500

    codewar solution

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,5 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    sonYearsOld = sonYearsOld * 2
+    return Math.abs(dadYearsOld - sonYearsOld)
+}
+//https://www.codewars.com/kata/5b853229cfde412a470000d0
