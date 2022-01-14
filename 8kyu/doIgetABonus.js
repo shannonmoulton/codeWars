ff2868b7c915c49ff2c57830b34c514e33cd33c2
Shannon Moulton <moulton.shannon@gmail.com>
2022-01-13 22:06:55 -0500

    added missing code

--- 8kyu/doIgetABonus.js
+++ 8kyu/doIgetABonus.js
@@ -1,1 +1,9 @@
+function bonusTime(salary, bonus) {
+    console.log(salary, bonus)
+    if (bonus === true) {
+        return "£" + salary * 10
+    } else {
+        return "£" + salary
+    }
+}
 //https://www.codewars.com/kata/56f6ad906b88de513f000d96
