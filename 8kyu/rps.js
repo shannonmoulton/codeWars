8f67c61709c0a1932c5ef1ca29185bb2f27d4040
Shannon Moulton <moulton.shannon@gmail.com>
2022-01-29 14:22:31 -0500

    codewar solution

---
+++ 8kyu/rps.js
@@ -0,0 +1,10 @@
+const rps = (p1, p2) => {
+    if (p1 === 'rock' && p2 === 'scissors' || p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock') {
+        return 'Player 1 won!'
+    } if (p2 === 'rock' && p1 === 'scissors' || p2 === 'scissors' && p1 === 'paper' || p2 === 'paper' && p1 === 'rock') {
+        return 'Player 2 won!'
+    } if (p1 === p2) {
+        return 'Draw!'
+    }
+};
+//https://www.codewars.com/kata/5672a98bdbdd995fad00000f
