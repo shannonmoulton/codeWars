750a59e1e6281a7162588dc46ca32791b748d920
Shannon Moulton <moulton.shannon@gmail.com>
2022-02-13 21:39:25 -0500

    codewar solution

---
+++ 8kyu/areYouPlayingBanjo.js
@@ -0,0 +1,4 @@
+function areYouPlayingBanjo(name) {
+    return name[0] === 'R' || name[0] === 'r' ? name + " plays banjo" : name + " does not play banjo"
+}
+//https://www.codewars.com/kata/53af2b8861023f1d88000832
