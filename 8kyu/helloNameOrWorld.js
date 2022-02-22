37c967d7af4ecca1d3446e3b3024904c72b0d26f
Shannon Moulton <moulton.shannon@gmail.com>
2022-02-22 10:32:39 -0500

    codewar solution

---
+++ 8kyu/helloNameOrWorld.js
@@ -0,0 +1,6 @@
+function hello(name) {
+    return name ? "Hello, " + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase() + "!"
+        : "Hello, World!"
+}
+
+//https://www.codewars.com/kata/57e3f79c9cb119374600046b
