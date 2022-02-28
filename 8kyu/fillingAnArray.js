aeb97e68e3767448b1da53fd6c9033683de9e88c
Shannon Moulton <moulton.shannon@gmail.com>
2022-02-27 21:52:52 -0500

    codewar solution

---
+++ 8kyu/fillingAnArray.js
@@ -0,0 +1,9 @@
+const arr = N => {
+    let array = []
+    for (let i = 0; i < N; i++) {
+        array.push(i)
+    }
+    return array
+};
+
+//https://www.codewars.com/kata/571d42206414b103dc0006a1
