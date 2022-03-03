b3bbb32364cfa0cce39d7c6bfc4d5b4e3651bf09
Shannon Moulton <moulton.shannon@gmail.com>
2022-03-02 21:29:37 -0500

    codewar solution

---
+++ 8kyu/didSheSayHallo.js
@@ -0,0 +1,5 @@
+function validateHello(greetings) {
+    res = (/hello|ciao|salut|hallo|hola|ahoj|czesc/i).test(greetings)
+    return res
+}
+//https://www.codewars.com/kata/56a4addbfd4a55694100001f
