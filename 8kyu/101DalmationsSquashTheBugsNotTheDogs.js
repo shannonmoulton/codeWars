8d40d9a09c7446189ef73ac256918eeff62600ce
Shannon Moulton <moulton.shannon@gmail.com>
2022-03-03 09:41:47 -0500

    codewar solution

---
+++ 8kyu/101DalmationsSquashTheBugsNotTheDogs.js
@@ -0,0 +1,12 @@
+function howManyDalmatians(number) {
+
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+
+    var respond = number <= 10 ? dogs[0]
+        : number <= 50 ? dogs[1]
+            : number == 101 ? dogs[3] : dogs[2]
+
+    return respond
+}
+
+//https://www.codewars.com/kata/56f6919a6b88de18ff000b36
