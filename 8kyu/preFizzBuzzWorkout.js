77404c3b9456c1f8430e982f808d9e6757b1b4f3
Shannon Moulton <moulton.shannon@gmail.com>
2022-03-19 14:40:50 -0400

    codewar solution

---
+++ 8kyu/preFizzBuzzWorkout.js
@@ -0,0 +1,8 @@
+function preFizz(n) {
+    let array = []
+    for (let i = 1; i < n + 1; i++) {
+        array.push(i)
+    }
+    return array
+}
+//https://www.codewars.com/kata/569e09850a8e371ab200000b
