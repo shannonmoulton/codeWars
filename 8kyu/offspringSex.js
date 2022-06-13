26d3dee53cf497644731fed5d35b3d69d2a42042
Shannon Moulton <moulton.shannon@gmail.com>
2022-06-12 22:18:05 -0400

    codewar solution

---
+++ 8kyu/offspringSex.js
@@ -0,0 +1,5 @@
+function chromosomeCheck(sperm) {
+    return sperm === 'XX' ? "Congratulations! You're going to have a daughter." : "Congratulations! You're going to have a son.";
+}
+
+//https://www.codewars.com/kata/56530b444e831334c0000020
