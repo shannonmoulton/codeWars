adac8b1a896ddf289e5cf8972c7f9870dce510e5
Shannon Moulton <moulton.shannon@gmail.com>
2022-06-26 22:36:19 -0400

    codewar solution

---
+++ 8kyu/alanPartridgIIappleTurnover.js
@@ -0,0 +1,4 @@
+function apple(x) {
+    return Number(x) ** 2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+// https://www.codewars.com/kata/580a094553bd9ec5d800007d
