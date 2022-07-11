e0e0f2ae686db5a1511db6b4c90b8ff1b2283af7
Shannon Moulton <moulton.shannon@gmail.com>
2022-07-11 13:17:42 -0400

    codewar solution

---
+++ 8kyu/basicDataTypesArray.js
@@ -0,0 +1,20 @@
+function getLength(arr) {
+    return arr.length
+}
+function getFirst(arr) {
+    return arr[0]
+}
+function getLast(arr) {
+    return arr[arr.length - 1]
+}
+function pushElement(arr) {
+    var el = 1;
+    arr.push(el)
+    return arr
+}
+function popElement(arr) {
+    arr.pop()
+    return arr
+}
+
+//https://www.codewars.com/kata/571effabb625ed9b0600107a
