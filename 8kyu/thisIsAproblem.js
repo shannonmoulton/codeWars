d0b947115b55ec6d0ee13f7b2c206da7cf88d1ca
Shannon Moulton <moulton.shannon@gmail.com>
2022-07-21 19:09:55 -0400

    codewar solution

---
+++ 8kyu/thisIsAproblem.js
@@ -0,0 +1,7 @@
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+    return this.name = `${first} ${last}`;
+}
+
+//https://www.codewars.com/kata/547c71fdc5b2b38db1000098
