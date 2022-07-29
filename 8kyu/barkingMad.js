fcfb64761528b65a493c66bb19d85d10ff15e488
Shannon Moulton <moulton.shannon@gmail.com>
2022-07-28 22:17:08 -0400

    codewar solution

---
+++ 8kyu/barkingMad.js
@@ -0,0 +1,17 @@
+function Dog(breed) {
+    this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function () {
+    return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function () {
+    return "Woof";
+};
+
+//https://www.codewars.com/kata/54dba07f03e88a4cec000caf
