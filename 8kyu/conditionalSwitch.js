f98f1469a1d0cd806e96c4a5798e3955a9e286f7
Shannon Moulton <moulton.shannon@gmail.com>
2022-08-05 22:23:10 -0400

    codewar solution

---
+++ 8kyu/conditionalSwitch.js
@@ -0,0 +1,23 @@
+function howManydays(month) {
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+            break;
+        case 2:
+            return 28;
+    }
+}
+
+//https://www.codewars.com/kata/572059afc2f4612825000d8a
