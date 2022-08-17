ce472f1a4aad7c1199a647719b8e50d113e0d3cd
Shannon Moulton <moulton.shannon@gmail.com>
2022-08-17 16:14:33 -0400

    codewar solution

---
+++ 8kyu/thinkfulNumberDrills.js
@@ -0,0 +1,6 @@
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    return (blueStart - bluePulled) / (redStart - redPulled + blueStart - bluePulled)
+
+}
+
+//https://www.codewars.com/kata/5862f663b4e9d6f12b00003b
