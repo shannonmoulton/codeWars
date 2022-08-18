378eb2cbbd3f58a24bdc32f436fbf97d1a8e0414
Shannon Moulton <moulton.shannon@gmail.com>
2022-08-18 16:15:22 -0400

    codewar solution

---
+++ 8kyu/switchCaseBugFixing.js
@@ -0,0 +1,19 @@
+function evalObject(value) {
+    var result = 0;
+    switch (value.operation) {
+        case '+': result = value.a + value.b;
+            break;
+        case '-': result = value.a - value.b;
+            break;
+        case '/': result = value.a / value.b;
+            break;
+        case '*': result = value.a * value.b;
+            break;
+        case '%': result = value.a % value.b;
+            break;
+        case '^': result = Math.pow(value.a, value.b);
+    }
+    return result;
+}
+
+//https://www.codewars.com/kata/55c933c115a8c426ac000082
