94288f294caa701937bdfe0ef328ef87b3af56c1
Shannon Moulton <moulton.shannon@gmail.com>
2022-08-22 16:17:47 -0400

    codewar solution

---
+++ 8kyu/forTwins2MathOperations.js
@@ -0,0 +1,5 @@
+function iceBrickVolume(radius, bottleLength, rimLength) {
+    return (bottleLength - rimLength) * 2 * radius * radius
+}
+
+//https://www.codewars.com/kata/59c287b16bddd291c700009a
