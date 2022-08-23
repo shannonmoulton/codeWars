c6a292710eec43d66556ddb8f93a4f149214267b
Shannon Moulton <moulton.shannon@gmail.com>
2022-08-23 16:20:01 -0400

    codewar solution

---
+++ 8kyu/fixTheBugsMyFirstKata.js
@@ -0,0 +1,8 @@
+function myFirstKata(a, b) {
+    if (typeof a != "number" || typeof b != "number") {
+        return false
+    } else
+        return (a % b) + (b % a);
+}
+
+//https://www.codewars.com/kata/56aed32a154d33a1f3000018
