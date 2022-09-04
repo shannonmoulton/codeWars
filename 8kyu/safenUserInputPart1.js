cbc89847b6ac1081a8ce39322af6e5907105398f
Shannon Moulton <moulton.shannon@gmail.com>
2022-09-03 21:32:45 -0400

    codewar solution

---
+++ 8kyu/safenUserInputPart1.js
@@ -0,0 +1,24 @@
+function htmlspecialchars(formData) {
+    let arr = [];
+    for (let i = 0; i < formData.length; i++) {
+        switch (formData[i]) {
+            case '<':
+                arr.push('&lt;');
+                break;
+            case '>':
+                arr.push('&gt;');
+                break;
+            case '"':
+                arr.push('&quot;');
+                break;
+            case '&':
+                arr.push('&amp;');
+                break;
+            default:
+                arr.push(formData[i]);
+        }
+    }
+    return arr.join('');
+}
+
+//https://www.codewars.com/kata/56bcaedfcf6b7f2125001118
