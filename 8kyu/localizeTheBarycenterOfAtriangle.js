e763714f8da1d9502f3a8f820afb8cedda454810
Shannon Moulton <moulton.shannon@gmail.com>
2022-09-09 21:37:01 -0400

    codewar solution

---
+++ 8kyu/localizeTheBarycenterOfAtriangle.js
@@ -0,0 +1,6 @@
+function barTriang(p1, p2, p3) {
+    let x = +((p1[0] + p2[0] + p3[0]) / 3).toFixed(4), y = +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4)
+    return [x, y]
+}
+
+//https://www.codewars.com/kata/5601c5f6ba804403c7000004
