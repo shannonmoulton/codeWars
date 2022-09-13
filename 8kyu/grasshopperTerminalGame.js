4912468e3888b129d0470e0b1d7217507c6d8599
Shannon Moulton <moulton.shannon@gmail.com>
2022-09-12 21:39:22 -0400

    codewar solution

---
+++ 8kyu/grasshopperTerminalGame.js
@@ -0,0 +1,9 @@
+function Hero(name) {
+    this.name = name || 'Hero';
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+}
+
+//https://www.codewars.com/kata/55e8aba23d399a59500000ce
