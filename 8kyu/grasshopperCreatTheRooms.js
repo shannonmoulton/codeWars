e3bca7ae279e435cc279ce0f9f9a4e2b99ca66b6
Shannon Moulton <moulton.shannon@gmail.com>
2022-09-13 21:40:07 -0400

    codewar solution

---
+++ 8kyu/grasshopperCreatTheRooms.js
@@ -0,0 +1,18 @@
+var rooms = {
+    1: {
+        name: 'name',
+        description: 'description',
+        completed: 'completed'
+    },
+    2: {
+        name: 'name',
+        description: 'description',
+        completed: 'completed'
+    },
+    3: {
+        name: 'name',
+        description: 'description',
+        completed: 'completed'
+    }
+}
+//https://www.codewars.com/kata/56a29b237e9e997ff2000048
