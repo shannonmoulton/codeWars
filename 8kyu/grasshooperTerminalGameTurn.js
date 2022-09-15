a90dea4458080ae595e1fa37551d06e4c0f83597
Shannon Moulton <moulton.shannon@gmail.com>
2022-09-14 21:40:45 -0400

    codewar solution

---
+++ 8kyu/grasshooperTerminalGameTurn.js
@@ -0,0 +1,10 @@
+function doTurn() {
+    rollDice();
+    move();
+    combat();
+    getCoins();
+    buyHealth();
+    printStatus();
+}
+
+//https://www.codewars.com/kata/56019d3b2c39ccde76000086
