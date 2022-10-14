458f0c9d5835468bc2c81416555f82e0e206ad01
Shannon Moulton <moulton.shannon@gmail.com>
2022-10-13 22:34:53 -0400

    codewar solution

---
+++ 8kyu/funWithEs6Classes1.js
@@ -0,0 +1,16 @@
+class Person {
+    constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+        this.firstName = firstName,
+            this.lastName = lastName,
+            this.age = age,
+            this.gender = gender
+    }
+    sayFullName() {
+        return this.firstName + ' ' + this.lastName
+    }
+    static greetExtraTerrestrials(raceName) {
+        return `Welcome to Planet Earth ${raceName}`
+    }
+}
+
+//https://www.codewars.com/kata/56f7f8215d7c12c0e7000b19
