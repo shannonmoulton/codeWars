48fc177510a5d3c3de29f9833a9efba0183749d0
Shannon Moulton <moulton.shannon@gmail.com>
2022-10-23 08:24:52 -0400

    codewar solution

---
+++ 8kyu/forUfcFans.js
@@ -0,0 +1,6 @@
+var quote = function (fighter) {
+    fighter = fighter.toLowerCase()
+    return fighter === 'george saint pierre' ? "I am not impressed by your performance." : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
+
+//https://www.codewars.com/kata/582dafb611d576b745000b74
