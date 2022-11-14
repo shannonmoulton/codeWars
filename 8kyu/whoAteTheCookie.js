fad9b879bc268645d92de9d62e81542e7b8e46b4
Shannon Moulton <moulton.shannon@gmail.com>
2022-11-13 19:02:21 -0500

    codewar solution

---
+++ 8kyu/whoAteTheCookie.js
@@ -0,0 +1,7 @@
+function cookie(x) {
+    return typeof x === 'string' ? "Who ate the last cookie? It was Zach!"
+        : typeof x === 'number' ? "Who ate the last cookie? It was Monica!"
+            : "Who ate the last cookie? It was the dog!"
+}
+
+//https://www.codewars.com/kata/55a996e0e8520afab9000055
