a5e796bcba3dd8438521eb5bf5dc24fd519d5bea
Shannon Moulton <moulton.shannon@gmail.com>
2022-11-28 18:04:05 -0500

    codewar solution

---
+++ 8kyu/speedCode3.js
@@ -0,0 +1,7 @@
+class Labrador {
+    constructor(name, age, gender, master) {
+        Object.assign(this, { name, age, gender, master, species: "Labrador", size: "Large", legs: 4, size: "Large", loyal: true })
+    }
+}
+
+//https://www.codewars.com/kata/56ff9b53140fcca90b000530
