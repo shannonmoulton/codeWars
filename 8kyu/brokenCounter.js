62b15aea19a69384068862e9fbd7a54ff59975c6
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-03 16:54:27 -0500

    codewar solution

---
+++ 8kyu/brokenCounter.js
@@ -0,0 +1,17 @@
+function Counter() {
+    this.value = 0;
+}
+
+Counter.prototype.increase = function () {
+    this.value++;
+};
+
+Counter.prototype.getValue = function () {
+    return this.value;
+};
+
+Counter.prototype.reset = function () {
+    this.value = 0;
+};
+
+//https://www.codewars.com/kata/526471539d52735c620000c6
