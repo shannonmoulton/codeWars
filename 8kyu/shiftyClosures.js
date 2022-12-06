322bd50b7900580064e7c1ea634dbbf5abc764bc
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-06 13:54:09 -0500

    codewar solution

---
+++ 8kyu/shiftyClosures.js
@@ -0,0 +1,11 @@
+var name = 'Abe';
+var greet_abe = function () {
+    return "Hello, " + name + '!';
+};
+
+var greet_ben = function () {
+    name = 'Ben';
+    return "Hello, " + name + '!';
+};
+
+//https://www.codewars.com/kata/514aa0dc21607ae236000017
