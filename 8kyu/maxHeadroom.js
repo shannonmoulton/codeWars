02902f54675b8bf560b79cb99f6f6fe612fd65b5
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-08 00:20:15 -0500

    codewar solution

---
+++ 8kyu/maxHeadroom.js
@@ -0,0 +1,13 @@
+function getMax1() {
+    var max =
+    {
+        name: 'Max Headroom'
+    }
+    return max;
+}
+
+function getMax2() {
+    return getMax1()
+}
+
+//https://www.codewars.com/kata/52a47dd7e950edabfa000391
