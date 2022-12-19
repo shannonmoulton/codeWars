711b7ada30c82757badcc0bebdc37e91c3917e03
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-19 10:07:49 -0500

    codewar solution

---
+++ 7kyu/refactoredGreeting.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    greet(yourName) {
+        return "Hello " + yourName + ", my name is " + this.name;
+    }
+}
+
+//https://www.codewars.com/kata/5121303128ef4b495f000001
