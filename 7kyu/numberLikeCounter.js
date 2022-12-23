7d9952c82cfb7dd5decee3f4bdfddb7020ca45a6
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-23 00:21:24 -0500

    codewar solution

---
+++ 7kyu/numberLikeCounter.js
@@ -0,0 +1,16 @@
+class Counter {
+    constructor() {
+        this.counter = 0;
+    }
+
+    incr() {
+        this.counter++;
+    }
+
+    valueOf() {
+        return this.counter;
+    }
+}
+
+
+//https://www.codewars.com/kata/5313b713bb244a0eb20001fe
