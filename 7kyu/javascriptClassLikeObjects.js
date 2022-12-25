d1d396e3886d8c6a5a6e2a85467e6ba0f271e476
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-24 19:53:16 -0500

    codewar solution

---
+++ 7kyu/javascriptClassLikeObjects.js
@@ -0,0 +1,11 @@
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    toString() {
+        return `${this.name} is a ${this.type}`
+    }
+}
+
+//https://www.codewars.com/kata/513e1e47c600c93cef000001
