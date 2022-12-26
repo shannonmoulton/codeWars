34741dd62ca17de61eaabfc4b4959899385a08b5
Shannon Moulton <moulton.shannon@gmail.com>
2022-12-25 19:52:02 -0500

    codewar solution

---
+++ 7kyu/javascriptNamespacing.js
@@ -0,0 +1,10 @@
+MyNamespace.MyClass = class {
+    constructor(arg) {
+        this.arg = arg
+    }
+    sayHello() {
+        return this.arg
+    }
+}
+
+//https://www.codewars.com/kata/524c6a2e55025e3fba00020f
